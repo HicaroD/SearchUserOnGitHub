b106df241c179188ad892abb6daac1a479e0a2c6
HicaroD <hdanrlley1@gmail.com>
2023-03-07 11:18:43 -0300

    feat(data): adding `index.ts` to export HttpClient interface

--- src/data/repositories/get_github_user_repository/get_github_user_from_api.ts
+++ src/data/repositories/get_github_user_repository/get_github_user_from_api.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "../../contracts/http_client/http_client";
+import { HttpClient } from "../../contracts/http_client";
 
 export class GetGitHubUserFromAPI {
   constructor(private readonly httpClient: HttpClient) {}
