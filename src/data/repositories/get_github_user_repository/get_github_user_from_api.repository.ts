d2a7682ae782a1529f70935dea7c827a171618ec
HicaroD <hdanrlley1@gmail.com>
2023-03-08 11:06:23 -0300

    feature: implementing repository interface on data layer

--- src/data/repositories/get_github_user_repository/get_github_user_from_api.repository.ts
+++ src/data/repositories/get_github_user_repository/get_github_user_from_api.repository.ts
@@ -1,5 +1,13 @@
+import { GitHubUserEntity } from "../../../domain/entities";
+import { GetGitHubUserRepository } from "../../../domain/repositories";
+import { GetGitHubUserDTO } from "../../../domain/usecases";
 import { HttpClient } from "../../contracts/http_client";
 
-export class GetGitHubUserFromAPI {
+export class GetGitHubUserFromAPI implements GetGitHubUserRepository {
   constructor(private readonly httpClient: HttpClient) {}
+
+  async getGitHubUser(params: GetGitHubUserDTO): Promise<GitHubUserEntity> {
+    // TODO: Implement communication with API
+    throw new Error("Method not implemented.");
+  }
 }
